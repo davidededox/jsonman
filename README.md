c5bf0bdf9518624d71dbaadf4787284429fb84a3
Davide Bedin <davidebedin@MacBookPro.local>
2022-06-08 00:46:51 +0200

    My first try

---
+++ README.md
@@ -0,0 +1,3 @@
+# json-server
+
+# json-server
---
+++ index.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('data.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+//const port = process.env.PORT || 3000; // <== You can change the port
+const port = 3000; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+console.log('SERVER LISTENING ON PORT 3000')
+
+server.listen(port);
