7a0d872e69e91cc7457e7e0ab17396e0c9444c2f
Davide Bedin <davidebedin@MacBookPro.local>
2022-06-25 00:53:33 +0200

    2306

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; // <== You can change the port
-//const port = 3000; // <== You can change the port
+//const port = process.env.PORT // <== You can change the port
+const port = 3000; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
