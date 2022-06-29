235af430abcfcad2ac0b4c30465a4c0590d501e3
Davide Bedin <davidebedin@MacBookPro.local>
2022-06-29 18:02:09 +0200

    2906

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('data.json'); // <== Will be created later
+const router = jsonServer.router("data.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT // <== You can change the port
+const port = process.env.PORT; // <== You can change the port
 //const port = 3000; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
 
-console.log('SERVER LISTENING ON PORT 3000')
+console.log("SERVER LISTENING ON PORT 3000");
 
 server.listen(port);
